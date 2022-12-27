9a36586e077725832fc1c27a2d79482614309d8a
Azim Ulmasov <azimulmasov2@gmail.com>
2022-12-27 20:06:00 +0500

    descending

--- logic18.py
+++ logic18.py
@@ -5,4 +5,13 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x_1 = a % 10
+    x_2 = a // 10 % 10
+    x_3 = a // 100 % 10
+    x_4 = a // 1000 % 10
+    x_5 = a // 10000
+    
+    answer = x_5 < x_4 and x_4 < x_3 and x_3 < x_2 and x_2 < x_1
+    return answer
+
+print(main(75421))
