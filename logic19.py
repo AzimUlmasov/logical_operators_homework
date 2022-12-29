7d5971d5707f82cbcf406848be9f75f3af915165
Azim Ulmasov <azim.ulmasov@saf.local>
2022-12-29 09:51:16 +0500

    palindrome

--- logic19.py
+++ logic19.py
@@ -8,4 +8,9 @@ def main(x):
     Returns:
         bool: answer
     """
-    return
+    # answer = (x // 100 < 1 and x // 10 == x % 10) or (x // 100 >= 1 and x // 100 == x % 10)
+    answer = ((x > 9 and x < 100) and x % 11 == 0) or (x > 99 and x // 100 == x % 10)
+    return answer
+
+print(main(121))
+
