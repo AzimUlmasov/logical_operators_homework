724d57e7a85cd8566a517d63cdae7e86301d7536
AzimUlmasov <azimulmasov2@gmail.com>
2022-12-29 10:29:13 +0500

    5 digit n

--- logic20.py
+++ logic20.py
@@ -9,4 +9,24 @@ def main(n):
         bool: answer
     """
 
-    return
+    d_1 = n % 10
+    d_2 = n // 10 % 10
+    d_3 = n // 100 % 10
+    d_4 = n // 1000 % 10
+    d_5 = n // 10000
+ 
+    one_count = d_1 == 1
+    one_count += d_2 == 1
+    one_count += d_3 == 1
+    one_count += d_4 == 1
+    one_count += d_5 == 1
+
+    zero_count = d_1 == 0 and (d_2 or d_3 or d_4 or d_5)
+    zero_count += d_2 == 0 and (d_3 or d_4 or d_5)
+    zero_count += d_3 == 0 and (d_4 or d_5)
+    zero_count += d_4 == 0 and d_5
+    
+    answer = one_count > zero_count
+    return answer
+
+print(main(10011))
